33872be526ce6217f3e9c1be3660a7a476866557
Joseph Ribbe <social@coderwelsch.com>
2020-09-01 17:17:33 +0200

    Initial gatsby-config setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,41 @@
+const WebMConverter = require("");
+
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		siteUrl: `https://www.go-n-grow.org`,
+		title: `Go ‘n‘ Grow!`,
+		description: `Samen pflanzen. Kiez verschönern. Insekten retten. Es ist soooo einfach!`,
+		location: "Potsdam",
+		author: `Joseph Ribbe, Coderwelsch`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `files`,
+				path: `${ __dirname }/src/`,
+			},
+		},
+		`gatsby-transformer-video`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Go ‘n‘ Grow!`,
+				short_name: `GnG`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				// icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		`gatsby-plugin-sass`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
+};
