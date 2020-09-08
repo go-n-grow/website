fbdb70cc762ebb004958d82f280b5af9bbc3254b
Joseph Ribbe <social@coderwelsch.com>
2020-09-08 16:05:28 +0200

    Added icon comp

---
+++ src/components/ui/atom/icon/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { cn } from "reusable-components/helper";
+
+import Styles from "./index.module.scss";
+
+
+const Icon = ({ className, size, icon }) =>
+	<i className={ cn(
+		Styles.icon,
+		className,
+		`icon-${ icon }`,
+		size && `is-size${ size }` // bulma text sizes
+	) } />;
+
+Icon.propTypes = {
+	icon: PropTypes.oneOf([
+		"logo",
+		"explanation",
+		"location"
+	]),
+	size: PropTypes.oneOf([1, 2, 3, 4, 5, 6])
+};
+
+export default Icon;
